46c83e5ed1a1eeedda5e3a599a3c17b2f940b568
sonia <112455045+soniarai4@users.noreply.github.com>
2022-09-01 03:26:39 +0000

    completed exercise 1

---
+++ ch01/exercises/simple.py
@@ -0,0 +1,17 @@
+print(10*5)
+print(10**2)
+print(15/10)
+print(15//10)
+print(-15//10)
+print(15%10)
+print(10%15)
+print(10%10)
+print(0%10)
+print(10/15) #this answer is an infinite decimal
+print("What is the exchange rate for a Euro to Dollar?")
+rate = input()
+print("How much do you want to exchange?")
+amount = input()
+total= amount*rate
+result=total-3
+print("{result}")
