ee4914cda8a91a9c1c74be60dfb5f388d0d86df1
sonia <112455045+soniarai4@users.noreply.github.com>
2022-09-03 03:43:57 +0000

    fixed part 2 of "working with data" activity

--- ch01/exercises/simple.py
+++ ch01/exercises/simple.py
@@ -8,10 +8,8 @@ print(10%15)
 print(10%10)
 print(0%10)
 print(10/15) #this answer is an infinite decimal
-print("What is the exchange rate for a Euro to Dollar?")
-rate = input()
-print("How much do you want to exchange?")
-amount = input()
-total= amount*rate
-result=total-3
-print("{result}")
+rate = int(input("What is the exchange rate for Euro to Dollar?"))
+amount = int(input("How much do you want to exchange?"))
+total = amount/rate
+result = total-3
+print(result)
