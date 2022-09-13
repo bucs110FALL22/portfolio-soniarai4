22ce28e5f225a73638034b6b89bff1d64a20ed39
sonia <112455045+soniarai4@users.noreply.github.com>
2022-09-13 03:47:45 +0000

    completed ch02 exercise: drawing with turtle

---
+++ ch02/exercises/drawing.py
@@ -0,0 +1,22 @@
+import turtle
+my_turtle = turtle.Turtle()
+
+turtle.delay(75)
+my_turtle.shape("turtle")
+my_turtle.color("hot pink")
+
+
+
+
+sides = int(input("Enter the number of sides:"))
+length = int(input("Enter the length of the sides:"))
+turn = ((sides-2)*180/sides)
+if turn > 90:
+  turn = turn % 90
+  turn= 90 - turn
+
+n = 0
+for n in range (0, sides):
+  my_turtle.fd(length)
+  my_turtle.right(turn)
+  n + 1
