cd206a110ab716a3d815ab9e5da03bca0aedcfbe
sonia <112455045+soniarai4@users.noreply.github.com>
2022-09-29 02:24:00 +0000

    ch04 exercise

--- ch04/exercises/dictionaries.py
+++ ch04/exercises/dictionaries.py
@@ -1,10 +1,10 @@
 article = "Adam Levine of the band Maroon 5 has been accused of having an affair after an Instagram model named Sumner Stroh posted a series of purported DMs in a TikTok video on September 19. These stunning details were revealed just one week after his wife, Behati Prinsloo, told the world that they were expecting their third child."
 
-substitutions = {"purported" : "suggestive",
+subs = {"purported" : "suggestive",
   "of the band Maroon 5" : "the adulterer",
   "accused" : "convicted",
   "stunning" : "gruesome"}
 
-for key, value in substitutions.items():
+for key, value in subs.items():
   article = article.replace(key,value)
   print(article)
