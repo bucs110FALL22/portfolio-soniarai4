e836b09cb239b378233360f9b5a72a3613715df7
sonia <112455045+soniarai4@users.noreply.github.com>
2022-09-29 02:21:44 +0000

    ch03 exercise: substitutions

--- ch02/exercises/myturtle.py
+++ ch02/exercises/myturtle.py
@@ -3,34 +3,26 @@ my_turtle = turtle.Turtle()
 
 turtle.delay(50)
 
-turtle.fd(50)
-my_turtle.color("maroon")
+
+my_turtle.color("purple")
 
 my_turtle.shape("turtle")
 
-my_turtle.fd(50)
-my_turtle.right(90)
-my_turtle.fd(50)
-my_turtle.right(90)
-my_turtle.fd(50)
-my_turtle.right(90)
-my_turtle.fd(50)
+for i in range(4):
+  my_turtle.fd(50)
+  my_turtle.right(90)
+
 
 my_turtle.up()
-my_turtle.forward(20)
+my_turtle.forward(100)
 my_turtle.down()
 
-my_turtle.color("pale violet red")
-
-my_turtle.left(90)
-my_turtle.fd(50)
-my_turtle.right(90)
-my_turtle.fd(50)
-my_turtle.right(90)
-my_turtle.fd(50)
-my_turtle.right(90)
-my_turtle.fd(50)
-
-
-
 my_turtle.color("red")
+ 
+for i in range(4):
+  my_turtle.fd(50)
+  my_turtle.right(90)
+
+
+
+
