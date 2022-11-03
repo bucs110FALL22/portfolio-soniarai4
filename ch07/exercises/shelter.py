40ad59210ea01ab9c70479b7b06d8f5e1b1a035a
sonia <112455045+soniarai4@users.noreply.github.com>
2022-11-03 04:34:58 +0000

    completed ch07 exercise: shelter

---
+++ ch07/exercises/shelter.py
@@ -0,0 +1,24 @@
+import time
+
+class Animal:
+  def __init__(self, name, type):
+    self.id = time.strftime("%d%m%M%S")
+    self.name = name
+    self.type = type
+    self.arrived = time.strftime("%d/%m/%Y")
+    self.adopted = None
+
+  def set_adopted(self):
+    self.adopted = time.strftime("%d/%m/%Y")
+
+  def __str__(self):
+    result_str = f"{self.name}[{self.type}]"
+    result_str += f"\narrived: {self.arrived}"
+    result_str += f"\nadopted:{self.adopted}"
+    return result_str
+
+def main():
+  fido = Animal("fido", "mouse")
+  fido.set_adopted()
+  print(fido)
+main()
